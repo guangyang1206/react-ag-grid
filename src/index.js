8f0a97701c79e21abcffc618af25dc24b5f908cb
Guang Yang <Guang_Yang@epam.com>
2019-04-03 16:51:59 +0800

    React redux and ag grid integration example finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render}from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/store.js';
+import FileView from './FileView.js';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+const rootDiv = document.getElementById('root');
+
+const comp =
+  <div>
+    <div className={'btn-padding'}>
+    <button onClick={reloadComponent}>Reload Component</button>
+    </div>
+    <Provider store={store}>
+      <FileView/>
+    </Provider>
+  </div>;
+
+render(comp, rootDiv);
+
+function reloadComponent() {
+  ReactDOM.unmountComponentAtNode(rootDiv);
+  // adding a slight delay so that reloading is noticeable!
+  setTimeout(() => render(comp, rootDiv), 50);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
